d1dd8e3dfb5a71ab53e5fea9a003f56cc3ff179d
bjay97 <bimsara.j@metarunelabs.dev>
2025-10-13 16:06:48 +0530

    feat: creating an empty skill node to represent potential skill node area

--- src/app/testing/page.jsx
+++ src/app/testing/page.jsx
@@ -1,10 +1,12 @@
 ﻿import SkillNode from "@/components/skillNode";
+import EmptySkillNode from "@/components/emptySkillNode";
 
 export default function testingPage(){
     return(
         <div className={"grid grid-cols-1 gap-3 justify-center place-items-center"}>
             <SkillNode/>
             <SkillNode/>
+            <EmptySkillNode/>
             <SkillNode/>
         </div>
     )
---
+++ src/components/emptySkillNode.jsx
@@ -0,0 +1,9 @@
+﻿export default function EmptySkillNode(){
+    return(
+        <button className={"cursor-pointer"}>
+            <div className={"flex flex-col bg-gray-400 rounded-full max-h-32 max-w-32"}>
+                <span className={"text-xl"}>empty</span>
+            </div>
+        </button>
+    )
+}
