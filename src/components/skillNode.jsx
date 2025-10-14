ba5c3d0114c5c79a5d364d9f1bcad45a81abd6db
bjay97 <bimsara.j@metarunelabs.dev>
2025-10-14 15:38:01 +0530

    feat: set up delete button on hover for filled node

--- src/components/skillNode.jsx
+++ src/components/skillNode.jsx
@@ -1,12 +1,9 @@
 ﻿export default function SkillNode({node}){
     return(
-        <button
-        className={"cursor-pointer"}
-        >
-            <div className={"flex flex-col bg-yellow-300 rounded-full max-h-32 max-w-32"}>
-                <span className={"text-2xl"}>{node.title}</span>
-                <span>{node.desc}</span>
-            </div>
-        </button>
+        <div className={"relative group flex flex-col bg-yellow-300 rounded-full max-h-48 max-w-48 flex items-center justify-center"}>
+            <span className={"text-xl text-black"}>{node.title}</span>
+            <span className={"text-black"}>{node.desc}</span>
+            <button className={"justify-center flex items-center absolute top-1 right-1 cursor-pointer opacity-0 group-hover:opacity-100 transition-opacity bg-red-300 rounded-full hover:bg-red-700"}>X</button>
+        </div>
     )
 }
