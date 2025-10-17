8cd00a39f945ee7b6cd465c46e1b613295d6f7d8
bjay97 <bimsara.j@metarunelabs.dev>
2025-10-17 08:06:02 +0530

    feat: setting up edit node modal

--- src/app/testing/page.jsx
+++ src/app/testing/page.jsx
@@ -1,9 +1,10 @@
 ﻿import SkillTree from "@/components/skillTree";
+import EditModal from "@/components/editModal";
 
-export default function testingPage(){
-    return(
+export default function testingPage() {
+    return (
         <div className={"grid grid-cols-1 gap-3 justify-center place-items-center"}>
-            <SkillTree/>
+            <EditModal/>
         </div>
     )
 }
---
+++ src/components/editModal.jsx
@@ -0,0 +1,21 @@
+﻿export default function EditModal() {
+    return (
+        <div className={"flex flex-col items-center"}>
+            <div className={"flex flex-col items-center"}>
+                <span>Title</span>
+                <input
+                    type={"text"}
+                    placeholder={"Enter Title"}
+                />
+                <span>Description</span>
+                <textarea
+                    placeholder={"Enter Description"}
+                />
+            </div>
+            <div className={"flex flex-row gap-2"}>
+                <button className={"cursor-pointer"}>Confirm</button>
+                <button className={"cursor-pointer"}>Cancel</button>
+            </div>
+        </div>
+    )
+}
