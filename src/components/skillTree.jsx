c39172c834c709463a2ce977afc77f76e1e84e02
bjay97 <bimsara.j@metarunelabs.dev>
2025-10-17 11:15:32 +0530

    chore: getting the id from skillTree for edit click

--- src/components/skillTree.jsx
+++ src/components/skillTree.jsx
@@ -39,7 +39,7 @@ export default function SkillTree({nodes, setNodes, onEdit}) {
         <div className={"grid grid-cols-1 gap-4"}>
             {nodes.map(node =>
                 node.filled ? (<SkillNode key={node.id} node={node}
-                                          onEditClick={onEdit}
+                                          onEditClick={onEdit(node.id)}
                                           onDeleteClick={() => handleDeletion(node.id)}/>) : (
                     <EmptySkillNode key={node.id} onClick={() => handleEmptyNodeClick(node.id)}/>))}
             <button
